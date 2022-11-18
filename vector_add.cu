4d9f9a6552b6f50e38e4ce3743ddd6f4616bf5ce
azheng_umass_edu <azheng_umass_edu@gypsum-gpu080.unity.rc.umass.edu>
2022-11-18 02:23:22 +0000

    finished cuda 01

--- vector_add.cu
+++ vector_add.cu
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define N 10000000
+#define N 100000
 
 __global__ void vector_add(float *out, float *a, float *b, int n) {
     for (int i = 0; i < n; i++) {
@@ -10,21 +10,30 @@ __global__ void vector_add(float *out, float *a, float *b, int n) {
 
 int main() {
     float *a, *b, *out;
-    float *d_a;
+    float *d_a, *d_b, *d_out;
 
-    // allocate device memory for a
+    // allocate host memory
     a   = (float*)malloc(sizeof(float) * N);
     b   = (float*)malloc(sizeof(float) * N);
     out = (float*)malloc(sizeof(float) * N);
 
-    // Initialize array
+    // allocate device memory
+    cudaMalloc((void**)&d_a, sizeof(float) * N);
+    cudaMalloc((void**)&d_b, sizeof(float) * N);
+    cudaMalloc((void**)&d_out, sizeof(float) * N);
+
+    // initialize array on device
     for(int i = 0; i < N; i++) {
         a[i] = 1.0f; b[i] = 2.0f;
-    }
+    } // redundancy?
 
+    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
     // Main function
-    vector_add<<<1,1>>>(out, a, b, N);
+    vector_add<<<1,1>>>(d_out, d_a, d_b, N);
 
+    cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
+       
     for (int i = 0; i < 10; ++i) {
    	    printf("a: %f, b: %f, o: %f\n", a[i], b[i], out[i]);
     }
