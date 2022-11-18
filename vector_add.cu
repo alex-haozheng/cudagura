05a735f8614e1c003bdef825a84fa5fba25ce4bf
azheng_umass_edu <azheng_umass_edu@gypsum-gpu080.unity.rc.umass.edu>
2022-11-18 01:16:20 +0000

    vector add for cuda

--- vector_add.cu
+++ vector_add.cu
@@ -2,30 +2,30 @@
 #include <stdio.h>
 #define N 10000000
 
-void vector_add(float *out, float *a, float *b, int n) {
-    for(int i = 0; i < n; i++){
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    for (int i = 0; i < n; i++) {
         out[i] = a[i] + b[i];
     }
 }
 
-int main(){
-    float *a, *b, *out; 
+int main() {
+    float *a, *b, *out;
+    float *d_a;
 
-    // Allocate memory
+    // allocate device memory for a
     a   = (float*)malloc(sizeof(float) * N);
     b   = (float*)malloc(sizeof(float) * N);
     out = (float*)malloc(sizeof(float) * N);
 
     // Initialize array
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N; i++) {
         a[i] = 1.0f; b[i] = 2.0f;
     }
 
     // Main function
-    vector_add(out, a, b, N);
+    vector_add<<<1,1>>>(out, a, b, N);
 
-
-    for (int i = 0; i < 10; ++i) {	
+    for (int i = 0; i < 10; ++i) {
    	    printf("a: %f, b: %f, o: %f\n", a[i], b[i], out[i]);
     }
 }
