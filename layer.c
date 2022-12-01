2be29f0607dd9cc468acc5770b6eee516bad064e
azheng_umass_edu <azheng_umass_edu@login2.unity.rc.umass.edu>
2022-12-01 18:46:35 +0000

    dec 1 comments

---
+++ layer.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	// example of CSR
+	// edges: (0, 1) (0, 2) (0, 3) (1, 7) (1, 4) (3, 5)
+	// offset arr: [0, 3, 5, 5, 6]
+	// value arr: [1, 2, 3, 7, 4, 5]
+	
+	// len(offset) = n + len(value) = len(edges) ?
+	
+	
+}
