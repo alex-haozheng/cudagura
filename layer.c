34f63550115b5fef2131791e2263810dfa74a5d5
azheng_umass_edu <azheng_umass_edu@gypsum-gpu038.unity.rc.umass.edu>
2022-12-02 08:57:20 +0000

    functioned it

--- layer.c
+++ layer.c
@@ -1,17 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-	// example of CSR
-	// edges: (0, 1) (0, 2) (0, 3) (1, 7) (1, 4) (3, 5)
-	// offset arr: [0, 3, 5, 5, 6]
-	// value arr: [1, 2, 3, 7, 4, 5]
-
-	// todo: generate edges from offset arr & value arr
-	int offset[] = {0, 3, 5, 5, 6};
-	int values[] = {1, 2, 3, 4, 7, 5};
-	int edges[sizeof(values)/sizeof(*values)];
-	int len = sizeof(offset)/sizeof(*offset);
+void to_csr(int offset[], int values[], int len) {	
+//	int edges[sizeof(values)/sizeof(*values)];
 	for (int i = 0; i < len; ++i) {
 		if (i + 1 < len) {
 			for (int j = offset[i]; j < offset[i+1]; ++j) {
@@ -23,5 +14,19 @@ int main() {
 			}
 		}
 	}	
+}
+
+
+int main() {
+	// example of CSR
+	// edges: (0, 1) (0, 2) (0, 3) (1, 7) (1, 4) (3, 5)
+	// offset arr: [0, 3, 5, 5, 6]
+	// value arr: [1, 2, 3, 7, 4, 5]
+
+	// todo: generate edges from offset arr & value arr
+	int o[] = {0, 3, 5, 5, 6};
+	int val[] = {1, 2, 3, 4, 7, 5};
 	
+	int l = sizeof(o)/sizeof(*o);
+	to_csr(o, val, l);
 }
