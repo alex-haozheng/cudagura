7cffd79057e8374476603ee308c9dccf64aa997b
azheng_umass_edu <azheng_umass_edu@ials-gpu002.unity.rc.umass.edu>
2022-12-25 02:32:22 +0000

    added write to three files (indptr, indices, graphCharacteristics)

--- loader.py
+++ loader.py
@@ -9,20 +9,23 @@ split_idx = dataset.get_idx_split()
 # there is only one graph in Node Property Prediction datasets
 g, labels = dataset[0]
 # get edges a tensor tuple
-print(g.edges())
 
 csr = dgl.graph(g.edges()).adj_sparse('csr')
+
 # csr contains (indptr, indices, edge_ids)
 # i plan to write them on different lines
-with open('sample.csv' , 'w') as f:
+
+with open('../data/indptr' , 'wb') as f:
   # using numpy()
   # edges = tensor.numpy()
   csr[0].numpy().astype('int16').tofile(f)
-  f.write('\n')
+
+with open('../data/indices', 'wb') as f:
   csr[1].numpy().astype('int16').tofile(f)
-  f.write('\n')
-  csr[2].numpy().astype('int16').tofile(f)
-# done?
+
+# todo: number of nodes edges and seperate into 3 files
+with open('../data/graph', 'w') as f:
+  f.write(f'{g.num_nodes()} {g.num_edges()}')
 
 train_label = dataset.labels[split_idx['train']]
 valid_label = dataset.labels[split_idx['valid']]
