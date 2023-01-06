81989baa18c5c5084d0165b2b960c88d1bca37a4
azheng_umass_edu <azheng_umass_edu@login1.unity.rc.umass.edu>
2023-01-06 03:43:43 +0000

    finish sampling code

--- layer.cpp
+++ layer.cpp
@@ -10,32 +10,37 @@
 using namespace std;
 
 typedef struct block {
-	vector<float> offset;
-	vector<float> values;
-	vector<float> unique;
+	vector<int> offset;
+	vector<int> values;
+	vector<int> unique;
 } block;
 
+typedef struct graphStruct {
+	vector<int> indptr;
+	vector<int> indices;
+} graphStruct;
+
 //loop on offset as threshold
-void to_graph(float offset[], float values[], int len) {	
+void to_graph(int offset[], int values[], int len) {	
 	//	int edges[sizeof(values)/sizeof(*values)];
 	for (int i = 0; i < len - 1; ++i) {
 		for (int j = offset[i]; j < offset[i+1]; ++j) {
-			printf("%d, %f\n", i, values[j]); 
+			printf("%d, %d\n", i, values[j]); 
 		}
 	}	
 }
 
-void to_csr(vector<vector<float>> graph) {
+void to_csr(vector<vector<int>> graph) {
 	// len(offset) == num_nodes + 1 (will hardcode this for now)
-	float offset[5];
+	int offset[5];
 	int len = graph.size();
-	float indices[len];
+	int indices[len];
 	int currN = -1;
 	int ind = -1;
 	// loop through to len
 	for (int i = 0; i < len; ++i) {
 		while (graph[i][0] != currN) {
-			printf("graph value %f\n", graph[i][0]);
+			printf("graph value %d\n", graph[i][0]);
 			currN = (int) graph[i][0];
 			cout << "index: " << ind << "\n";
 			offset[++ind] = i;
@@ -44,69 +49,53 @@ void to_csr(vector<vector<float>> graph) {
 	}
 	offset[++ind] = len;
 	for (int i = 0; i < 5; ++i) {
-		printf("offset %d: %f\n", i, offset[i]);
+		printf("offset %d: %d\n", i, offset[i]);
 	}
 	for (int i = 0; i < len; ++i) {
-		printf("indices %d: %f\n", i, indices[i]);
+		printf("indices %d: %d\n", i, indices[i]);
 	}
 }
 
 // todo: add one more parameter: vector<int> targetNodes
-void sample_layer(vector<vector<float>> graph, struct block* t_block) {
-	map<float, vector<float>> m;
-	//loop for graph (tested)
-	for (vector<float> vect : graph) {
-		m[vect[0]].push_back(vect[1]);
-  }
-	t_block->values = {};
-	for (float x : t_block->unique) {
-		auto search = m.find(x);
-		if (search != m.end()) {
-			t_block->values.insert(t_block->values.end(), search->second.begin(), search->second.end());
-		}
-	}
-	t_block->unique = t_block->values;
+void sample_layer(struct graphStruct* graph, struct block* t_block, vector<int> nodes) {
+	// for (int x : t_block->unique) {
+	// 	auto search = m.find(x);
+	// 	if (search != m.end()) {
+	// 		t_block->values.insert(t_block->values.end(), search->second.begin(), search->second.end());
+	// 	}
+	// }
 	// have all the values
+	int offset = 0;
+	for (int x: nodes) {
+		t_block->offset.push_back(offset);
+		for (int i = graph->indptr[x]; i < graph->indptr[x+1]; ++i, ++offset) {
+			t_block->values.push_back(graph->indices[i]);
+		}
+	} t_block->offset.push_back(offset);
+
+	t_block->unique = t_block->values;
 	sort(t_block->unique.begin(), t_block->unique.end());
 	cout << t_block->unique.size();
-	vector<float>::iterator ip = unique(t_block->unique.begin(), t_block->unique.begin() + t_block->unique.size());
+	vector<int>::iterator ip = unique(t_block->unique.begin(), t_block->unique.begin() + t_block->unique.size());
 
 	t_block->unique.resize(distance(t_block->unique.begin(), ip));
 
 	for (ip = t_block->unique.begin(); ip != t_block->unique.end(); ++ip) {
     cout << *ip << " ";
   }
-  // for(map<float, vector<float> >::const_iterator it = m.begin();
-	// 	it != m.end(); ++it)
-	// {
-	// 		std::cout << it->first << "\n" << "values: ";
-
-	// 		for (int i = 0; i < it->second.size(); ++i) {
-	// 			cout << it->second[i] << ' ';
-	// 		}
-	// 		cout << endl;
-	// }
 }
 
 int main() {
 
-	float o[] = {0, 3, 5, 5, 6};
-	float val[] = {1, 2, 3, 4, 7, 5};
-
-	int l = sizeof(o)/sizeof(*o);
-	//to_graph(o, val, l);
-
-	printf("\n");
-
-	vector<vector<float>> g = {{0, 1}, {0, 2}, {0, 3}, {1, 4}, {1, 7}, {3, 5}};
-	struct block b = { {0, 3, 5, 5, 6}, {1, 2}, {1, 2}};
+	printf("\nsampling has started :) \n");
 
 	block arr[4];
 	vector<int> targetNodes = {1,2,3,4};
-	arr[0].unique = targetNodes
+	arr[0].unique = targetNodes;
 	// to_csr(g);
-	for (int i = 0; i < 3; ++i) {
-		// sampole_layer(graph, &block[i+1], block[i].uniqyu
-	}
-	sample_layer(g, &b);
+	// todo: fill graphStruct while reading binary
+	// what to do with graph characteristic file (for output?)
+	// for (int i = 0; i < 3; ++i) {
+	// 	// sample_layer(&g, &block[i+1], block[i].unique);
+	// }
 }
